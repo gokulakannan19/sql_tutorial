08d35e858c867cc358ec798dd1fc8f21c4012476
Gokula Kannan <gokulakannan.r@idp.com>
2023-07-25 20:34:34 +0530

    insert update delete operation

---
+++ 18_iNSERT_MULTIPLE_ROWS.sql
@@ -0,0 +1,11 @@
+USE sql_store;
+
+-- Insert 3 rows in the products table
+INSERT INTO
+    products (name, quantity_in_stock, unit_price)
+VALUES
+    ("Jelly", 10, 10.5)
+VALUES
+    ("Egg", 30, 5.5)
+VALUES
+    ("Maggi", 70, 4.5)
---
+++ 19.create_copy.sql
@@ -0,0 +1,17 @@
+USE sql_invoicing
+CREATE TABLE
+    invoices_archieved AS
+SELECT
+    i.invoice_id,
+    i.number,
+    c.name AS client,
+    i.invoice_total,
+    i.payment_total,
+    i.invoice_date,
+    i.payment_date,
+    i.due_date
+FROM
+    invoices i
+    JOIN clients c USING (client_id)
+WHERE
+    i.payment_date IS NOT NULL;
