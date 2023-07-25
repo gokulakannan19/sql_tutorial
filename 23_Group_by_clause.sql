4acd240e055e0d5e8b5e32b0962796f7719dbb7a
Gokula Kannan <gokulakannan.r@idp.com>
2023-07-25 22:28:02 +0530

    aggregate functions completed

---
+++ 23_Group_by_clause.sql
@@ -0,0 +1,14 @@
+USE sql_invoicing;
+
+SELECT
+    p.date,
+    pm.name AS payment_method,
+    SUM(p.amount) AS total_payments
+FROM
+    payments p
+    JOIN payment_methods pm ON p.payment_method = pm.payment_method_id
+GROUP BY
+    p.date,
+    p.payment_method
+ORDER BY
+    p.date;
